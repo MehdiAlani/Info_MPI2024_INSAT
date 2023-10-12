f8e831e364e2865e6d8a847d75360d354f5d2086
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-12 08:09:04 -0200

    12 oct 23

--- TD/TD2/Exercice7.c
+++ TD/TD2/Exercice7.c
@@ -1,27 +1,37 @@
 #include <stdio.h>
-
-
+#include <stdlib.h>
 int main(){
-    int nb = 0;
-    short Valide;
-    char c;
-    char *message;
+    int N;
+    char Texte[512];
+    short int Valid = 1;
+    int nbchar = 0, nbmots=0;
     do{
+        printf("Donner Le nombre de mots a compter: ");
+        scanf("%d",&N);
+        printf("\n");
+    }while(N < 0);
+    for(int i=0;i<512;i++){
+        Texte[i]='#';
+    } 
     do{
-        c = getchar();
-        message[nb] = c;
-        nb++;
-    }while(c != '#');
-    printf("\n");
-    Valide=1;
-    for(int i = 0; i<nb;i++){
-        if(message[i]<'A' || message[i]>'Z' && message[i] < 'a' || message[i] > 'z' && message[i]!='\n' && message[i]!=' '){
-            Valide=0;
-            break;
+        printf("Donner Vortre Texte :");
+        scanf("%s",&Texte);
+        for(int i=0; Texte[i]!='#'; i++){
+            if('A'>Texte[i] && Texte[i]!=' '|| 'Z'<Texte[i] && 'a'>Texte[i] || 'z'<Texte[i] && Texte[i]!='\n') Valid = 0;
+        } 
+    }while(Valid==0);
+    for(int i =0; i != '#'; i++){
+        if(Texte[i]==' '){
+            if(nbchar == N){
+                nbmots++;
+            }
+            nbchar=0;
         }
-        printf("%c",message[i]);
+        else{
+            nbchar++;
+        }
     }
-    }while(Valide == 0);
+    printf("Le Nombre de mots Avec %d caracteres sont %d\n",N,nbchar);
 
     return 0;
 }
