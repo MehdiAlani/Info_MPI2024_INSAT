160007ca2b4a2d2fa4049dc8e3102e0f4b063f32
Kyymo <137647536+MehdiAlani@users.noreply.github.com>
2023-10-12 22:03:33 -0200

    12 oct 23

--- TD/TD2/Exercice7.c
+++ TD/TD2/Exercice7.c
@@ -1,37 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-    int N;
-    char Texte[512];
-    short int Valid = 1;
-    int nbchar = 0, nbmots=0;
-    do{
-        printf("Donner Le nombre de mots a compter: ");
-        scanf("%d",&N);
-        printf("\n");
-    }while(N < 0);
-    for(int i=0;i<512;i++){
-        Texte[i]='#';
-    } 
-    do{
-        printf("Donner Vortre Texte :");
-        scanf("%s",&Texte);
-        for(int i=0; Texte[i]!='#'; i++){
-            if('A'>Texte[i] && Texte[i]!=' '|| 'Z'<Texte[i] && 'a'>Texte[i] || 'z'<Texte[i] && Texte[i]!='\n') Valid = 0;
-        } 
-    }while(Valid==0);
-    for(int i =0; i != '#'; i++){
-        if(Texte[i]==' '){
-            if(nbchar == N){
-                nbmots++;
-            }
-            nbchar=0;
-        }
-        else{
-            nbchar++;
-        }
+    while(1){
+        printf("%c",getchar());
     }
-    printf("Le Nombre de mots Avec %d caracteres sont %d\n",N,nbchar);
-
-    return 0;
 }
